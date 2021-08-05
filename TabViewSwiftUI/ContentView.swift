c452f8ae98744f9172d6a02acfe9c4917403fbf5
Samsul Hadi <samsulhadikediri@gmail.com>
2021-08-06 05:35:30 +0700

    Initial Commit

---
+++ TabViewSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabViewSwiftUI
+//
+//  Created by Samsul Hadi on 06/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabViewSwiftUI/TabViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewSwiftUIApp.swift
+//  TabViewSwiftUI
+//
+//  Created by Samsul Hadi on 06/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TabViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
